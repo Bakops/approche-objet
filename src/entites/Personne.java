55b65a8929a8114cfe5d24c9de06311349de6181
Bakops <bakoutoure4@gmail.com>
2025-02-01 11:58:00 +0100

    PREMIER COMMIT

---
+++ src/entites/Personne.java
@@ -0,0 +1,8 @@
+package entites;
+
+public class Personne {
+    private String nom;
+    private String prenom;
+    private String adressepostal;
+
+}
---
+++ src/essais/TestAdressePostal.java
@@ -0,0 +1,13 @@
+
+package essais;
+
+import entites.AdressePostale;
+
+public class TestAdressePostale {
+    public static void main(String[] args) {
+
+        AdressePostale adresse1 = new AdressePostale(12, "Rue de Paris", "75001", "Paris");
+        AdressePostale adresse2 = new AdressePostale(45, "Avenue de la Liberté", "34000", "Montpellier");
+
+    }
+}
